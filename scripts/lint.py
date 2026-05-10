36ac1f5d7e4e6e7cbb63bf747ca341651c7e0a98
kyphan38 <kyphan.work@gmail.com>
2026-05-10 11:26:34 +0700

    feat: fix features

--- scripts/lint.py
+++ scripts/lint.py
@@ -7,7 +7,7 @@ from linter.structure import check_structure
 
 DOCS_DIR = "docs"
 KEBAB_CASE_PATTERN = re.compile(r'^[0-9a-z-]+$')
-IGNORE_DIRS = {".docusaurus", "node_modules", "img", ".git", ".gemini"}
+IGNORE_DIRS = {".docusaurus", "node_modules", "img", ".git", ".gemini", "07-content"}
 IGNORE_FILES = {os.path.normpath(os.path.join(DOCS_DIR, "00-introduction", "roadmap.md"))}
 
 
